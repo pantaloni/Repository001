7a6c21f239682b1432afe9c85bbbe3fd9eb2101b
laurent pantaloni <lpantaloni@free.fr>
2016-03-29 23:02:57 +0200

    Initial Commit

---
+++ gitTestTests/gitTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  gitTestTests.swift
+//  gitTestTests
+//
+//  Created by laurent pantaloni on 29/03/2016.
+//  Copyright © 2016 laurent pantaloni. All rights reserved.
+//
+
+import XCTest
+@testable import gitTest
+
+class gitTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
